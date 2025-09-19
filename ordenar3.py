f9dad076067facebab4210fa314a522e219bb4b8
lalaAlvarez <larisa.alvarez@iteso.mx>
2025-09-19 14:20:58 -0600

    numeros iguales

--- ordenar3.py
+++ ordenar3.py
@@ -1,7 +1,6 @@
 """
 Inserta el encabezado aquí y escribe tu código abajo
 """
-
 # Entradas
 numero1 = input("Primer número: ")
 numero2 = input("Segundo número: ")
@@ -12,26 +11,33 @@ tercero = 0
 
 
 # Proceso
-if numero1 > numero2:
-    segundo = numero1
-    if numero1 > numero3:
-        primero = numero1
-        if numero2 > numero3:
-            tercero = numero3
-            segundo = numero2
-        else:
-            tercero = numero2
-            segundo = numero3
-elif numero2 > numero3:
-    primero = numero2
-    segundo = numero3
-    if numero3 < numero1:
-        tercero = numero3
-        segundo = numero1
-else:
-    tercero = numero1
+
+if numero1 >= numero2:
+    primero = numero1
     segundo = numero2
-    primero = numero3
+    if numero2 >= numero3:
+        tercero = numero3
+    else:
+        segundo = numero3
+        tercero = numero2
+        if numero3 >= numero1:
+            primero = numero3
+            segundo = numero1
+            tercero = numero2
+else:
+    primero = numero2
+    segundo = numero1
+    if numero1 >= numero3:
+        tercero = numero3
+    else:
+        segundo = numero3
+        tercero = numero2
+        if numero3 >= numero2:
+            primero = numero3
+            segundo = numero2
+            tercero = numero1
+
+
 
 
 
